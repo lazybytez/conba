392c4dc35b2c77d84bc15f9a764c0eb33f0f446d
Pascal Zarrad <p.zarrad@outlook.de>
2026-04-10 16:47:32 +0200

    test(restic): move createBackups helper to helpers_test.go
    Consolidate shared test helpers into a single file.

--- internal/restic/forget_test.go
+++ internal/restic/forget_test.go
@@ -3,26 +3,10 @@ package restic_test
 import (
 	"context"
 	"errors"
-	"fmt"
 	"testing"
 
 	"github.com/lazybytez/conba/internal/restic"
 )
-
-func createBackups(t *testing.T, client *restic.Client, dataDir string, tags []string, count int) {
-	t.Helper()
-
-	ctx := context.Background()
-
-	for idx := range count {
-		createTestFile(t, dataDir, fmt.Sprintf("file%d.txt", idx), fmt.Sprintf("v%d", idx))
-
-		err := client.Backup(ctx, dataDir, tags)
-		if err != nil {
-			t.Fatalf("backup %d: %v", idx+1, err)
-		}
-	}
-}
 
 func TestForget_ReducesSnapshots(t *testing.T) {
 	repoPath, password := newTestRepo(t)
--- internal/restic/helpers_test.go
+++ internal/restic/helpers_test.go
@@ -1,6 +1,8 @@
 package restic_test
 
 import (
+	"context"
+	"fmt"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -45,5 +47,20 @@ func createTestFile(t *testing.T, dir string, name string, content string) {
 	err := os.WriteFile(filepath.Join(dir, name), []byte(content), 0o600)
 	if err != nil {
 		t.Fatal(err)
+	}
+}
+
+func createBackups(t *testing.T, client *restic.Client, dataDir string, tags []string, count int) {
+	t.Helper()
+
+	ctx := context.Background()
+
+	for idx := range count {
+		createTestFile(t, dataDir, fmt.Sprintf("file%d.txt", idx), fmt.Sprintf("v%d", idx))
+
+		err := client.Backup(ctx, dataDir, tags)
+		if err != nil {
+			t.Fatalf("backup %d: %v", idx+1, err)
+		}
 	}
 }
